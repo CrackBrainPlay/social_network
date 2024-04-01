3ad66ba1abe23ff335056bfdadb3a5d7ce8ef224
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-04-01 14:24:08 +0300

    Lesson 20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
             <div className={style.box}>
-                <div><a className={style.item} href="/Profile"><img className={style.img} src='img/profile.png' alt='Profile' />&nbsp;Profile</a></div>
-                <div><a className={style.item} href="/Dialogues"><img className={style.img} src='img/messages.png' alt='Messages' />&nbsp;Messages</a></div>
-                <div><a className={style.item} href="News"><img className={style.img} src='img/news.png' alt='News' />&nbsp;News</a></div>
-                <div><a className={style.item} href="Music"><img className={style.img} src='img/music.png' alt='Music' />&nbsp;Music</a></div>
+                {/* <div><NavLink activeClassName={style.item_activeLink} className={style.item} to="/Profile"><img className={style.img} src='img/profile.png' alt='Profile' />&nbsp;Profile</NavLink></div>
+                <div><NavLink className={style.item} to="/Dialogues"><img className={style.img} src='img/messages.png' alt='Messages' />&nbsp;Messages</NavLink></div>
+                <div><NavLink className={style.item} to="News"><img className={style.img} src='img/news.png' alt='News' />&nbsp;News</NavLink></div>
+                <div><NavLink className={style.item} to="Music"><img className={style.img} src='img/music.png' alt='Music' />&nbsp;Music</NavLink></div>
                 <br></br>
-                <div><a className={style.item} href="Settings"><img className={style.img} src='img/settings.png' alt='Settings' />&nbsp;Settings</a></div>
+                <div><NavLink className={style.item} to="Settings"><img className={style.img} src='img/settings.png' alt='Settings' />&nbsp;Settings</NavLink></div> */}
                 {/* <div className={style.item}><a className={style.btn} href="##">Settings</a></div> */}
+                <div className={style.item}><img className={style.img} src='img/profile.png' alt='Profile' /><NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to="/Profile">&nbsp;Profile</NavLink></div>
+                <div className={style.item}><img className={style.img} src='img/messages.png' alt='Messages' /><NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to="/Dialogues">&nbsp;Messages</NavLink></div>
+                <div className={style.item}><img className={style.img} src='img/news.png' alt='News' /><NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to="News">&nbsp;News</NavLink></div>
+                <div className={style.item}><img className={style.img} src='img/music.png' alt='Music' /><NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to="Music">&nbsp;Music</NavLink></div>
+                <br></br>
+                <div className={style.item}><img className={style.img} src='img/settings.png' alt='Settings' /><NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to="Settings">&nbsp;Settings</NavLink></div>
 
             </div>
         </nav>
