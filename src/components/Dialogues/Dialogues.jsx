af36450f061a415d182f5a263224363fe685b496
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-04-01 15:46:42 +0300

    Lesson 21

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -3,19 +3,31 @@ import style from './Dialogues.module.css';
 
 const Dialogues = (props) => {
     return (
-        <div className={style.content} >
-            Привет
-            <div className={style.item}>
-                My posts
+        <div className={style.dialogs} >
+            <div className={style.dialogs_items} >
                 <div className={style.item}>
-                    New post
+                    Петя
                 </div>
-                {/* <Posts message='How ar you ?' counterLikes='15' />
-                <Posts message="It's my first post" counterLikes='4' />
-                <Posts />
-                <Posts />
-                <Posts />
-                <Posts /> */}
+                <div className={style.item}>
+                    Вася
+                </div>
+                <div className={style.item}>
+                    Маша
+                </div>
+                <div className={style.item}>
+                    Гоша
+                </div>
+                <div className={style.item}>
+                    Саша
+                </div>
+                <div className={style.item}>
+                    Миша
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hello</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>Why do you not answer me?</div>
             </div>
         </div>
     );
