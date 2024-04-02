b3022a94328480f05fc21f7d4421646b0f1ed747
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-04-02 10:32:49 +0300

    Update 24

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import PersonProfile from './PersonProfile/PersonProfile';
 
 const DialogItem = (props) => {
-    let path = "/Dialogues/" + props.itemLink;
+    let path = "/Dialogues/" + props.id;
     return (
         <div className={style.item}>
             <NavLink className={({ isActive }) => isActive ? style.activeLink : ''} to={path}>
@@ -21,23 +21,36 @@ const Message = (props) => {
 }
 
 const Dialogues = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: 'Petr' },
+        { id: 2, name: 'Stas' },
+        { id: 3, name: 'Sasha' },
+        { id: 4, name: 'Masha' },
+        { id: 5, name: 'Pasha' },
+        { id: 6, name: 'Dima' }
+    ]
+
+    let massagesData = [
+        { id: 1, text: 'Hello' },
+        { id: 2, text: 'How are you?' },
+        { id: 3, text: 'Why do you not answer me?' }
+    ]
+
     return (
         <div className={style.dialogs} >
             <div className={style.dialogs_items} >
-                <DialogItem itemName='Petr' itemLink='1' />
-                <DialogItem itemName='Stas' itemLink='2' />
-                <DialogItem itemName='Sasha' itemLink='3' />
-                <DialogItem itemName='Masha' itemLink='4' />
-                <DialogItem itemName='Pasha' itemLink='5' />
-                <DialogItem itemName='Dima' itemLink='6' />
+                <DialogItem itemName={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem itemName={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem itemName={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogItem itemName={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogItem itemName={dialogsData[4].name} id={dialogsData[4].id} />
+                <DialogItem itemName={dialogsData[5].name} id={dialogsData[5].id} />
             </div>
             <div className={style.messages}>
-                <Message text='Hello' />
-                <Message text='How are you?' />
-                <Message text='Why do you not answer me?' />
-                {/* <div className={style.message}>Hello</div>
-                <div className={style.message}>How are you?</div>
-                <div className={style.message}>Why do you not answer me?</div> */}
+                <Message text={massagesData[0].text} id={dialogsData[0].id} />
+                <Message text={massagesData[1].text} id={dialogsData[2].id} />
+                <Message text={massagesData[2].text} id={dialogsData[3].id} />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,13 @@ import style from './MyPosts.module.css';
 import Posts from './Posts/Posts';
 
 const MyPosts = () => {
+
+    let postsData = [
+        { id: 1, text: 'How are you ?', counterLikes: 15 },
+        { id: 2, text: "It's my first post", counterLikes: 4 },
+        { id: 3, text: 'Why do you not answer me?', counterLikes: 76 }
+    ]
+
     return (
         <div className={style.content} >
             <div className={style.item}>
@@ -10,8 +17,9 @@ const MyPosts = () => {
                 <div className={style.item}>
                     New post
                 </div>
-                <Posts message='How ar you ?' counterLikes='15' />
-                <Posts message="It's my first post" counterLikes='4' />
+                <Posts message={postsData[0].text} counterLikes={postsData[0].counterLikes} />
+                <Posts message={postsData[1].text} counterLikes={postsData[1].counterLikes} />
+                <Posts message={postsData[2].text} counterLikes={postsData[2].counterLikes} />
                 {/* <Posts />
                 <Posts />
                 <Posts />
