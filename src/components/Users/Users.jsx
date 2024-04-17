76b81a8ed7696e40ed65531cee7543a92ba32c8f
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-04-17 17:10:33 +0300

    Update 54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,21 @@ import axios from 'axios';
 
 class User extends React.Component {
 
-    constructor(props) {
-        super(props);
-        // if (this.props.users.length === 0) {
+    // constructor(props) {
+    //     super(props);
+    // }
+    // if (this.props.users.length === 0) {
+    // axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    //     .then(response => {
+    //         this.props.setUsers(response.data.items)
+    //     });
+    // }
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             });
-        // }
     }
 
     // getUsers = () => {
