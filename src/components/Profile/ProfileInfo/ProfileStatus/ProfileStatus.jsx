a7a68c23ee3ec9350051ca3adfad64d80bf58347
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-05-03 16:45:00 +0300

    Update 74

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -26,6 +26,17 @@ class ProfileStatus extends React.Component {
         })
         this.props.updateStatus(this.state.status);
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        debugger;
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+        console.log('componentDidUpdate');
+    }
+
     render() {
         return (<div>
             {!this.state.editMode &&
